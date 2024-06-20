30b4dbfe0da9ba5ee4bcd14ee4085e024a93ad7e
Fabrice <schaubfabrice@gmail.com>
2024-06-20 16:12:49 +0200

    lol

--- src/common/vector.go
+++ src/common/vector.go
@@ -6,17 +6,17 @@ import (
 
 type Vector[T comparable] struct {
 	mtx  sync.Mutex
-	data *[]T
+	data []*T
 }
 
 func NewVector[T comparable]() *Vector[T] {
-	data := make([]T, 0)
+	data := make([]*T, 0)
 	return &Vector[T]{
-		data: &data,
+		data: data,
 	}
 }
 
-func Vectorize[T comparable](input_val []T) *Vector[T] {
+func Vectorize[T comparable](input_val []*T) *Vector[T] {
 	transformed := NewVector[T]()
 	for _, val := range input_val {
 		transformed.Push(val)
@@ -24,59 +24,59 @@ func Vectorize[T comparable](input_val []T) *Vector[T] {
 	return transformed
 }
 
-func (v *Vector[T]) Devectorize() []T {
-	arr := make([]T, v.Len())
-	arr = append(arr, *v.data...)
+func (v *Vector[T]) Devectorize() []*T {
+	arr := make([]*T, v.Len())
+	arr = append(arr, v.data...)
 	return arr
 }
 
-func (v *Vector[T]) Write(elements []T) {
+func (v *Vector[T]) Write(elements []*T) {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	*v.data = elements
+	v.data = elements
 }
 
-func (v *Vector[T]) Push(element T) {
+func (v *Vector[T]) Push(element *T) {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	*v.data = append(*v.data, element)
+	v.data = append(v.data, element)
 }
 
-func (v *Vector[T]) Read() []T {
+func (v *Vector[T]) Read() []*T {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	dataCopy := make([]T, len(*v.data))
-	copy(dataCopy, *v.data)
+	dataCopy := make([]*T, len(v.data))
+	copy(dataCopy, v.data)
 	return dataCopy
 }
 
 func (v *Vector[T]) Len() int {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	return len(*v.data)
+	return len(v.data)
 }
 
 func (v *Vector[T]) Clear() {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	*v.data = make([]T, 0)
+	v.data = make([]*T, 0)
 }
 
-func (v *Vector[T]) Get(index int) (T, bool) {
+func (v *Vector[T]) Get(index int) (*T, bool) {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	if index >= 0 && index < len(*v.data) {
-		return (*v.data)[index], true
+	if index >= 0 && index < len(v.data) {
+		return (v.data)[index], true
 	}
-	var zero T
+	var zero *T
 	return zero, false
 }
 
-func (v *Vector[T]) Update(index int, element T) bool {
+func (v *Vector[T]) Update(index int, element *T) bool {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	if index >= 0 && index < len(*v.data) {
-		(*v.data)[index] = element
+	if index >= 0 && index < len(v.data) {
+		(v.data)[index] = element
 		return true
 	}
 	return false
@@ -85,21 +85,21 @@ func (v *Vector[T]) Update(index int, element T) bool {
 func (v *Vector[T]) RemoveByIndex(index int) bool {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	if index >= 0 && index < len(*v.data) {
-		*v.data = append((*v.data)[:index], (*v.data)[index+1:]...)
+	if index >= 0 && index < len(v.data) {
+		v.data = append((v.data)[:index], (v.data)[index+1:]...)
 		return true
 	}
 	return false
 }
 
-func (v *Vector[T]) Pop() (T, bool) {
+func (v *Vector[T]) Pop() (*T, bool) {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
-	if len(*v.data) == 0 {
-		var zero T
+	if len(v.data) == 0 {
+		var zero *T
 		return zero, false
 	}
-	element := (*v.data)[len(*v.data)-1]
-	*v.data = (*v.data)[:len(*v.data)-1]
+	element := (v.data)[len(v.data)-1]
+	v.data = (v.data)[:len(v.data)-1]
 	return element, true
 }
--- src/main.go
+++ src/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"easyflow-ws/src/api"
 	"easyflow-ws/src/common"
+	"easyflow-ws/src/net"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func injectHub(h *net.Hub) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("net", h)
+		c.Next()
+	}
+}
+
 func main() {
 	config := common.LoadDefaultConfig()
 	logger := common.NewLogger(os.Stdout, "main")
+	hub := net.NewHub()
 
 	router := gin.Default()
+	router.Use(injectHub(hub))
+	router.GET("/ws", api.WebsocketListener)
 
 	logger.PrintfInfo("Starting ws-worker on port %s", config.Port)
 	router.Run(":" + config.Port)
